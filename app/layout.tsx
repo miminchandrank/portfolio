7ced3ef786e9254568168fcbab8f259e10c9a494
miminchandrank <miminchandrankalari@gmail.com>
2025-05-13 07:58:18 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,10 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: "Mimin Chandran K | Portfolio",
   description: "Data Scientist | AI/ML Engineer | Data Analyst",
-    generator: 'v0.dev'
+    generator: 'v0.dev',
+    icons: {
+      icon: "/favicon.ico",
+    },
 }
 
 export default function RootLayout({
@@ -23,6 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+       
       <body className={`${poppins.variable} font-sans`}>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           {children}
